2385c55016d47b816be7933060b4a60679d29828
Ludoes03 <ludoescrich@gmail.com>
2018-12-12 17:14:49 -0600

    Lista Productos

---
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,45 @@
+﻿namespace Sales.ViewModels
+{
+    using Common.Models;
+    using Sales.Services;
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using Xamarin.Forms;
+
+    public class ProductsViewModel: BaseViewModel
+    {
+        private ApiService apiService;
+
+        private ObservableCollection<Product> products;
+
+        public ObservableCollection<Product> Products
+        {
+            get { return this.products; }
+            set { this.SetValue(ref this.products, value); }
+        }
+
+        public ProductsViewModel()
+        {
+            apiService = new ApiService();
+            this.LoadProducts();
+        }
+
+        private async void LoadProducts()
+        {
+            var response = await this.apiService.GetList<Product>(
+                "http://192.168.4.149:82", 
+                "/api", 
+                "/Products");
+            
+            if(!response.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
+                return;
+            }
+
+            var list = (List<Product>)response.Result;
+            this.Products = new ObservableCollection<Product>(list);
+        }
+    }
+}
